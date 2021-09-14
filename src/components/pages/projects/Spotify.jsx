11c943efff1cb460793619d171f5caed53c0a3ba
warecuber <warecuber@gmail.com>
2021-09-14 16:35:50 -0500

    Fixed z-index issue with mobile nav

--- src/components/pages/projects/Spotify.jsx
+++ src/components/pages/projects/Spotify.jsx
@@ -9,14 +9,24 @@ const Spotify = () => {
 			bodyStyle={{ backgroundColor: "#1db954" }}
 			projectSummary={
 				<div>
-					<a
-						href="https://warecuber.github.io/SpotifyInfo/"
-						target="_Blank"
-						rel="noreferrer"
-						className="demo_link"
-					>
-						Working Product
-					</a>
+					<div className="project__links">
+						<a
+							href="https://warecuber.github.io/SpotifyInfo/"
+							target="_Blank"
+							rel="noreferrer"
+							className="demo_link"
+						>
+							Working Demo
+						</a>
+						<a
+							href="https://github.com/Warecuber/SpotifyInfo"
+							target="_Blank"
+							rel="noreferrer"
+							className="demo_link"
+						>
+							Source code
+						</a>
+					</div>
 					<p className="project__description">
 						I created the Spotify Stats because I wanted to see my most popular
 						songs/artists on Spotify, but did not want to aothenticate to a 3rd
