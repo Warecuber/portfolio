0382b2989542610307c0e2624d086c4508a4901e
warecuber <warecuber@gmail.com>
2021-09-13 20:07:50 -0500

    Initial commit

---
+++ src/components/ui/NavBar.jsx
@@ -0,0 +1,12 @@
+import { React } from "react";
+
+const NavBar = () => {
+	return (
+		<div className="header">
+			<div className="header__title">John Ware</div>
+			<div className="header__buttons"></div>
+		</div>
+	);
+};
+
+export default NavBar;
